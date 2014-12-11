784606c8f72b1ff00c16ab36d39588cf8870c5d3
Bread God <vipcube@gmail.com>
2014-12-11 22:01:11 +0800

    新增別的服務 OtherService

---
+++ Hello_GitHub/src/test/git/hub/other/OtherService.java
@@ -0,0 +1,12 @@
+package test.git.hub.other;
+
+public class OtherService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Yep, other Serivce");
+	}
+
+}
